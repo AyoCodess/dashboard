d4a7d7fc6b8e50ba475207e09ca88d0c7bd70561
ayocodess <ayo.daniel.adesanya@gmail.com>
2023-05-18 20:03:15 +0200

    updated menu items structure

--- app/sidebar.tsx
+++ app/sidebar.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import { useState } from 'react';
 import { Transition } from '@headlessui/react';
 import { FiChevronDown } from 'react-icons/fi';
@@ -8,8 +7,11 @@ import Link from 'next/link';
 type MenuItem = {
   id: number;
   label: string;
-  link: string;
-  children?: string[];
+  link?: string;
+  sumMenuItem?: {
+    label: string;
+    link: string;
+  }[];
 };
 
 const Sidebar = () => {
@@ -23,21 +25,45 @@ const Sidebar = () => {
     },
     {
       id: 2,
-      label: 'About',
-      link: '/about',
-      children: ['/Cars', '/Bikes']
+      label: 'Operations',
+      sumMenuItem: [
+        {
+          label: 'Cars',
+          link: '/cars'
+        },
+        {
+          label: 'Bus',
+          link: '/bus'
+        }
+      ]
     },
     {
       id: 3,
-      label: 'Product',
-      link: '/product',
-      children: ['/Cars', '/Bikes']
+      label: 'Statistics',
+      sumMenuItem: [
+        {
+          label: 'Cars',
+          link: '/cars'
+        },
+        {
+          label: 'Bus',
+          link: '/bus'
+        }
+      ]
     },
     {
       id: 4,
-      label: 'Bags',
-      link: '/bags',
-      children: ['/Cars', '/Bikes']
+      label: 'Services',
+      sumMenuItem: [
+        {
+          label: 'Cars',
+          link: '/cars'
+        },
+        {
+          label: 'Bus',
+          link: '/bus'
+        }
+      ]
     }
   ];
 
@@ -49,66 +75,72 @@ const Sidebar = () => {
     <div className="flex">
       <div className="w-1/4">
         <ul className="space-y-2">
-          {menuItems.map((menuItem) => (
-            <li
-              key={menuItem.id}
-              className="w-[14rem] rounded-md text-gray-700 py-2 px-4 cursor-pointer hover:bg-gray-100 transition-colors duration-200 ease-in-out "
-            >
-              <div
-                onClick={(e) => {
-                  e.preventDefault();
-                  toggleSubMenu(menuItem.id);
-                }}
-                className="flex items-center justify-between"
-              >
-                <Link
-                  href={menuItem.link}
-                  onClick={(e) => {
-                    e.stopPropagation();
-                  }}
-                >
-                  {menuItem.label}
-                </Link>
+          {menuItems.map((menuItem) => {
+            if (menuItem.link) {
+              return (
+                <li key={menuItem.id}>
+                  <Link
+                    href={menuItem.link}
+                    className="flex items-center justify-between"
+                  >
+                    <div>{menuItem.label}</div>
+                  </Link>
+                </li>
+              );
+            }
 
-                {menuItem.children && (
-                  <FiChevronDown
-                    className={`transform duration-200 ${
-                      openSubMenu === menuItem.id ? 'rotate-180' : ''
-                    }`}
-                  />
-                )}
-              </div>
-              {menuItem.children && (
-                <Transition
-                  show={openSubMenu === menuItem.id}
-                  enter="transition-all ease-in duration-200"
-                  enterFrom="opacity-0 max-h-0"
-                  enterTo="opacity-100 max-h-[200px]"
-                  leave="transition-all ease-out duration-200"
-                  leaveFrom="opacity-100 max-h-[200px]"
-                  leaveTo="opacity-0 max-h-0"
+            return (
+              <li
+                key={menuItem.id}
+                className="w-[14rem] rounded-md text-gray-700 py-2 px-4 cursor-pointer hover:bg-gray-100 transition-colors duration-200 ease-in-out "
+              >
+                <div
+                  onClick={(e) => {
+                    toggleSubMenu(menuItem.id);
+                  }}
+                  className="flex items-center justify-between"
                 >
-                  <ul className="pl-4">
-                    {menuItem.children.map((child, index) => (
-                      <Link href={child}>
+                  <div>{menuItem.label}</div>
+
+                  {menuItem.sumMenuItem && (
+                    <FiChevronDown
+                      className={`transform duration-200 ${
+                        openSubMenu === menuItem.id ? 'rotate-180' : ''
+                      }`}
+                    />
+                  )}
+                </div>
+                {menuItem.sumMenuItem && (
+                  <Transition
+                    show={openSubMenu === menuItem.id}
+                    enter="transition-all ease-in duration-200"
+                    enterFrom="opacity-0 max-h-0"
+                    enterTo="opacity-100 max-h-[200px]"
+                    leave="transition-all ease-out duration-200"
+                    leaveFrom="opacity-100 max-h-[200px]"
+                    leaveTo="opacity-0 max-h-0"
+                  >
+                    <ul className="pl-4">
+                      {menuItem.sumMenuItem.map((child, index) => (
                         <li
                           key={index}
                           className="text-gray-700 py-2 cursor-pointer hover:bg-gray-100 transition-colors duration-200 ease-in-out w-12"
                         >
-                          {child.substring(1)}
+                          <Link href={child.link} passHref>
+                            {child.label}
+                          </Link>
                         </li>
-                      </Link>
-                    ))}
-                  </ul>
-                </Transition>
-              )}
-            </li>
-          ))}
+                      ))}
+                    </ul>
+                  </Transition>
+                )}
+              </li>
+            );
+          })}
         </ul>
       </div>
       {/* Your main content goes here */}
     </div>
   );
 };
-
 export default Sidebar;
